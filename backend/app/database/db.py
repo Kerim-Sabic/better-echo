2fb4078b00cffa94c609e15fc8912219f8f8e3bd
Affan88888 <affankapidzic3@gmail.com>
2025-09-04 12:11:53 +0200

    Minor fix in database/db.py file

--- backend/app/database/db.py
+++ backend/app/database/db.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import declarative_base
 
 DATABASE_URL = "sqlite:///./database.db"
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False, "timeout": 10})
 Base = declarative_base()
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
